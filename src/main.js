281c4f91c39dcd43533583cddc14ef36ef42cb5f
leecason <leecason0722@gmail.com>
2019-12-09 20:51:24 +0800

    feat: ✨ export ElTiptapPlugin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 import ElTiptap from './components/ElTiptap.vue';
 
 export { ElTiptap };
+
+const ElTiptapPlugin = {
+  installed: false,
+
+  install (Vue) {
+    Vue.component('el-tiptap', ElTiptap);
+
+    this.installed = true;
+  },
+};
+
+export { ElTiptapPlugin };
