ffa7aa544717242798e7fc11743c166817e5f8d1
leecason <leecason0722@gmail.com>
2019-12-14 21:10:37 +0800

    migrate: 🚚 lib folder

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,10 @@ module.exports = {
   runtimeCompiler: true,
   chainWebpack: config => {
     config.resolve.alias
-      .set('@', path.resolve(__dirname, 'src'))
-      .set('utils', path.resolve(__dirname, 'src/utils'))
+      .set('@', path.resolve(__dirname, 'lib/src'))
       .set('examples', path.resolve(__dirname, 'examples'))
-      .set('el-tiptap', path.resolve(__dirname, 'src/main'));
+      .set('el-tiptap', path.resolve(__dirname, 'lib/index'))
+      .set('utils', path.resolve(__dirname, 'lib/src/utils'));
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/el-tiptap/'
