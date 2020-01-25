be16abd1bacaa22f533a48a5961a8a65b56a89ad
leecason <leecason0722@gmail.com>
2020-01-25 10:04:42 +0800

    chore: 🔧 .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,22 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'only-multiline'],
     'semi': ['error', 'always'],
+    'indent': ['error', 2, {
+      'SwitchCase': 1,
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3
+      }
+    }],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'no-multi-spaces': ['error', {
+      'exceptions': {
+        'VariableDeclarator': true,
+      },
+    }],
+    'space-before-function-paren': ['error', 'always'],
   },
   parserOptions: {
     parser: 'babel-eslint'
