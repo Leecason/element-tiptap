a99476ac75440096083d173a31752e20a89c0a2b
leecason <leecason0722@gmail.com>
2020-02-12 09:43:54 +0800

    refactor: ♻️  add table.ts extension file

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -20,7 +20,7 @@ export { default as OrderedList } from './ordered_list.ts';
 export { default as Image } from './image';
 export { default as TodoItem } from './todo_item';
 export { default as TodoList } from './todo_list';
-export { default as Table } from './table';
+export { default as Table } from './table.ts';
 export { default as Iframe } from './iframe';
 
 // marks
--- src/extensions/table.ts
+++ src/extensions/table.ts
@@ -1,8 +1,10 @@
 import { Table as TiptapTable } from 'tiptap-extensions';
+import { MenuData } from 'tiptap';
+import { MenuBtnView } from '../types';
 import TablePopover from '../components/MenuCommands/TablePopover/index.vue';
 
-export default class Table extends TiptapTable {
-  menuBtnView (editorContext) {
+export default class Table extends TiptapTable implements MenuBtnView {
+  menuBtnView (editorContext: MenuData) {
     return {
       component: TablePopover,
       componentProps: {
