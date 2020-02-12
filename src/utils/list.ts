462475b581fa0631e2c74fef9bb384ab38a6f95d
leecason <leecason0722@gmail.com>
2020-02-12 16:38:11 +0800

    fix: 🐛 correct isListNode parameters types

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,18 +1,18 @@
-import { NodeSpec } from 'prosemirror-model';
+import { Node } from 'prosemirror-model';
 
-export function isBulletListNode (node: NodeSpec): boolean {
+export function isBulletListNode (node: Node): boolean {
   return node.type.name === 'bullet_list';
 }
 
-export function isOrderedListNode (node: NodeSpec): boolean {
+export function isOrderedListNode (node: Node): boolean {
   return node.type.name === 'order_list';
 }
 
-export function isTodoListNode (node: NodeSpec): boolean {
+export function isTodoListNode (node: Node): boolean {
   return node.type.name === 'todo_list';
 }
 
-export function isListNode (node: NodeSpec): boolean {
+export function isListNode (node: Node): boolean {
   return isBulletListNode(node) ||
     isOrderedListNode(node) ||
     isTodoListNode(node);
