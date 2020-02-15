f102a7f37c42880db583372658a0e811e53223db
leecason <leecason0722@gmail.com>
2020-02-15 17:05:09 +0800

    config(lint): no-unsed-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard',
     '@vue/typescript'
@@ -12,23 +12,25 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'only-multiline'],
-    'semi': ['error', 'always'],
-    'indent': ['error', 2, {
-      'SwitchCase': 1,
-      'VariableDeclarator': {
-        'var': 2,
-        'let': 2,
-        'const': 3
+    semi: ['error', 'always'],
+    indent: ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: {
+        var: 2,
+        let: 2,
+        const: 3
       }
     }],
     'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
+    quotes: ['error', 'single'],
     'no-multi-spaces': ['error', {
-      'exceptions': {
-        'VariableDeclarator': true,
+      exceptions: {
+        VariableDeclarator: true,
       },
     }],
     'space-before-function-paren': ['error', 'always'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
