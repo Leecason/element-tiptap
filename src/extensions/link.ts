825d8ca675774409b1b5f4704d03e2bda4146056
leecason <leecason0722@gmail.com>
2020-03-11 13:17:09 +0800

    feat: ✨ select link when clicked

--- src/extensions/link.ts
+++ src/extensions/link.ts
@@ -1,9 +1,36 @@
+import { Plugin, TextSelection } from 'prosemirror-state';
 import { Link as TiptapLink } from 'tiptap-extensions';
+// @ts-ignore
+import { getMarkRange } from 'tiptap-utils';
 import { MenuData } from 'tiptap';
 import { MenuBtnView } from '@/../types';
 import LinkCommandButton from '@/components/MenuCommands/LinkCommandButton.vue';
 
 export default class Link extends TiptapLink implements MenuBtnView {
+  get plugins () {
+    return [
+      new Plugin({
+        props: {
+          handleClick (view, pos) {
+            const { schema, doc, tr } = view.state;
+
+            const range = getMarkRange(doc.resolve(pos), schema.marks.link);
+
+            if (!range) return false;
+
+            const $start = doc.resolve(range.from);
+            const $end = doc.resolve(range.to);
+
+            const transaction = tr.setSelection(new TextSelection($start, $end));
+
+            view.dispatch(transaction);
+            return true;
+          },
+        },
+      }),
+    ];
+  }
+
   menuBtnView (editorContext: MenuData) {
     return {
       component: LinkCommandButton,
