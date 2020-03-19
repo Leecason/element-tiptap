9e7f24166ede13bbecd9cc0d39dd44d25a5eebee
Bakir Aitkulov <bakir.aitkul@gmail.com>
2020-03-19 16:37:14 +0600

    feat: Add russiang i18n locale

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,12 +3,14 @@ import { warn } from '@/utils/console';
 import en from './en';
 import zh from './zh';
 import pl from './pl';
+import ru from './ru';
 
 const defaultLang = 'en';
 const dictionary = {
   en,
   zh,
   pl,
+  ru,
 };
 
 let currentLang: object = dictionary[defaultLang];
