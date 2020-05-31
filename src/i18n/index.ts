9f96e095551bd02976141586c7447574e995c6b2
Koas <alvaro.calleja@gmail.com>
2020-05-31 10:24:21 +0200

    Spanish translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -6,34 +6,36 @@ import pl from './pl';
 import ru from './ru';
 import de from './de';
 import ko from './ko';
+import es from './es';
 
 const defaultLang = 'en';
 const dictionary = {
-  en,
-  zh,
-  pl,
-  ru,
-  de,
-  ko,
+	en,
+	zh,
+	pl,
+	ru,
+	de,
+	ko,
+	es,
 };
 
 let currentLang: object = dictionary[defaultLang];
 
 export function useLang (l: string): void {
-  if (dictionary[l]) {
-    currentLang = dictionary[l];
-  } else {
-    Logger.warn(`Can't find the current language "${l}", Using language "${defaultLang}" by default. Welcome contribution to https://github.com/Leecason/element-tiptap`);
-    currentLang = dictionary[defaultLang];
-  }
+	if (dictionary[l]) {
+		currentLang = dictionary[l];
+	} else {
+		Logger.warn(`Can't find the current language "${l}", Using language "${defaultLang}" by default. Welcome contribution to https://github.com/Leecason/element-tiptap`);
+		currentLang = dictionary[defaultLang];
+	}
 }
 
 // @ts-ignore
 export function t (path: string): string | object {
-  const target = path.split('.').reduce((prev, curr) => {
-    // @ts-ignore
-    return prev[curr];
-  }, currentLang);
+	const target = path.split('.').reduce((prev, curr) => {
+		// @ts-ignore
+		return prev[curr];
+	}, currentLang);
 
-  return target;
+	return target;
 }
