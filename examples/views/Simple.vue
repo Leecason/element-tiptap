2d11b5cb585301f49b9f4756823a7283a5e7c266
leecason <leecason0722@gmail.com>
2020-06-10 23:33:06 +0800

    example: 🍱 update demo page content

--- examples/views/Simple.vue
+++ examples/views/Simple.vue
@@ -33,6 +33,7 @@ import {
   HardBreak,
   HorizontalRule,
   CodeView,
+  Fullscreen,
   History,
 } from 'element-tiptap';
 
@@ -74,10 +75,11 @@ export default {
             autoCloseTags: true,
           },
         }),
+        new Fullscreen(),
         new History(),
       ],
 
-      content: '<h2 data-text-align="center">Welcome To Element Tiptap Editor Demo</h2><p>🔥<strong>Element Tiptap Editor </strong>🔥is a WYSIWYG rich-text editor using&nbsp;<a href="https://github.com/scrumpy/tiptap" rel="noopener noreferrer nofollow">tiptap</a>&nbsp;and <a href="https://github.com/ElemeFE/element" rel="noopener noreferrer nofollow">element-ui</a>&nbsp;for Vue.js<image src="https://i.ibb.co/nbRN3S2/undraw-upload-87y9.png" alt="" title="" height="200" data-display="right"></image> that\'s easy to use, friendly to developers, fully extensible and clean in design.</p><p></p><p data-text-align="right">👉Click on the image to get started image features 👉</p><p></p><p><strong>Got questions or need help or feature request? </strong></p><p>🚀<strong>welcome to submit an <a href="https://github.com/Leecason/element-tiptap/issues" rel="noopener noreferrer nofollow">issue</a></strong> 😊</p><p>I\'m continuously working to add in new features.</p><p></p><blockquote><p>This demo is simple, switch tab for more features.</p><p>All demos source code: <a href="https://github.com/Leecason/element-tiptap/blob/master/examples/views/Index.vue" rel="noopener noreferrer nofollow">source code 🔗</a></p></blockquote>',
+      content: '<h2 style="text-align: center">Welcome To Element Tiptap Editor Demo</h2><p>🔥 <strong>Element Tiptap Editor </strong>🔥is a WYSIWYG rich-text editor using&nbsp; <a href="https://github.com/scrumpy/tiptap" rel="noopener noreferrer nofollow">tiptap</a>&nbsp;and <a href="https://github.com/ElemeFE/element" rel="noopener noreferrer nofollow">element-ui</a>&nbsp;for Vue.js <img src="https://i.ibb.co/nbRN3S2/undraw-upload-87y9.png" alt="" title="" height="200" data-display="right"> that\'s easy to use, friendly to developers, fully extensible and clean in design.</p><p></p><p style="text-align: right">👉Click on the image to get started image features 👉</p><p></p><p>You can switch to <strong>Code View </strong>💻 mode and toggle <strong>Fullscreen</strong> 📺 in this demo.</p><p></p><p><strong>Got questions or need help or feature request?</strong></p><p>🚀 <strong>welcome to submit an <a href="https://github.com/Leecason/element-tiptap/issues" rel="noopener noreferrer nofollow">issue</a></strong> 😊</p><p>I\'m continuously working to add in new features.</p><p></p><blockquote><p>This demo is simple, switch tab for more features.</p><p>All demos source code: <a href="https://github.com/Leecason/element-tiptap/blob/master/examples/views/Index.vue" rel="noopener noreferrer nofollow">source code 🔗</a></p></blockquote>',
     };
   },
 };
