b7c3fc58623c89f6414bb8cafa5578f30dde6e01
Leecason <leecason0722@gmail.com>
2022-07-06 13:48:23 +0800

    feat: build library and demo site with vite, remove rollup config

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset',
-  ],
-  plugins: [
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
-    ]
-  ],
+  presets: ['@vue/cli-plugin-babel/preset'],
 };
